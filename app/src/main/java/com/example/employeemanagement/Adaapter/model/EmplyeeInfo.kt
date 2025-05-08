ffc42694824f0292bdd3df1dac5769036d94c102
Ojaswakesharwani <ojaswakesharwani@gmail.com>
2025-05-09 03:17:51 +0530

    Fix issue of loading data becuase Gson won’t know how to match employee_name with name

--- app/src/main/java/com/example/employeemanagement/Adaapter/model/EmplyeeInfo.kt
+++ app/src/main/java/com/example/employeemanagement/Adaapter/model/EmplyeeInfo.kt
@@ -4,8 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class EmplyeeInfo(
     var id : Int,
-    var name : String,
-    var salary : Long,
-    var age :Int,
-    var profile :Int
+    @SerializedName("employee_name")
+    val name: String,
+
+    @SerializedName("employee_salary")
+    val salary: Int,
+
+    @SerializedName("employee_age")
+    val age: Int,
+
+    @SerializedName("profile_image")
+    val image: String
 )
